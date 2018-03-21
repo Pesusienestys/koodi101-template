7e184853561849dab67aafb45878ec331ef60332
Markus Sadeluoto <markus.sadeluoto@gmail.com>
2018-03-21 14:53:41 +0200

    yrk2

--- iot/phat.py
+++ iot/phat.py
@@ -9,7 +9,7 @@ import time
 def readSensors():
     sensors = {
         #"temperature": envirophat.weather.temperature(),
-        "pressure": envirophat.weather.pressure(),
+        "pressure": envirophat.weather.pressure()
         #"eu": "bbbbbbbbbbbbbbbbbbbbbbbbbb"
     }
 
