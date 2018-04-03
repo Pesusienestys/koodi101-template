52e7058ff6f5fe9ebb6019e71b92a16bf9b7b36b
Markus Sadeluoto <markus.sadeluoto@gmail.com>
2018-04-03 14:41:52 +0300

    kkk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -51,9 +51,36 @@ class App extends Component {
   }
 }
 
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+
 /****** DO NOT DELETE AFTER THIS LINE ******/
 
 ReactDOM.render(
   <App />,
+  <Toggle />,
   document.getElementById('root')
 );
