18c13f1e224a40ecf57eb7f7360cc8a1f8e14332
Arttu Miinalainen <arttu.miinalainen01@gmail.com>
2018-04-03 19:44:34 +0300

    l

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -30,8 +30,8 @@ const getTempFromBackend = async () => {
     console.log("Getting chat from "+url)
     const response2 = await fetch(url);
     const shit2 = response2.json()
-    const shit21 = shit2[shit2.length-1];
-    console.log(shit2)
+    var shit21 = shit2.seats[shit2.seats.length-1].shit21
+    console.log(shit21)
   } catch (error) {
     console.error(error);
   }
