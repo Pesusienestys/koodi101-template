e040c868eb5c6d0bc83366adfd7afe8c143c1579
Arttu Miinalainen <arttu.miinalainen01@gmail.com>
2018-04-04 13:23:18 +0300

    l

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -31,7 +31,7 @@ const getTempFromBackend = async () => {
     const response2 = await fetch(url);
     const shit2 = await response2.json()
     const shit21 = await shit2.results
-    array = shit21[shit21.length-1]
+    array = await shit21[shit21.length-1]
     console.log(array)
     return shit2.results
   } catch (error) {
